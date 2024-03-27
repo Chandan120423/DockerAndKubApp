fb90b8fd905d33bce9d2d27a2f94adad5a1747b0
Chandan <chandan52.hcu@gmail.com>
2024-03-27 16:06:14 +0530

    docker with kubernetes

---
+++ docker-demo/src/main/java/com/pes/dockerdemo/controller/DockerDemoController.java
@@ -0,0 +1,13 @@
+package com.pes.dockerdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerDemoController {
+
+    @GetMapping("/dockerTest")
+    public String dockerTest(){
+        return "Docker testing ...................";
+    }
+}
